c4566f57c499575f07553f368ada25f1fa1dc457
+ <ronob7898@yahoo.com>
2024-06-30 18:30:34 +0300

    Sets the Arithmetic right

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+ let num1 = 31;
+ let num2 = 2;
+ let multiply =num1 * num2;
+ console.log(multiply);
+
+//  Random integer > 0
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; 
+console.log(random)
+
+// Calculating mod
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+console.log(mod); 
+
+// Finding max
+let int1 = 10;
+let int2 = 15;
+let int3 = 17;
+let int4 = 20;
+let max = Math.max(int1, int2, int3, int4);
+console.log(max);
